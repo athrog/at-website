9dba7e31054f52424b333dd6eaf6ba5e11b93b39
Andy Throgmorton <athrog@users.noreply.github.com>
2022-01-19 17:24:56 -0800

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import logging
-import webapp2
-
-class HomePageHandler(webapp2.RequestHandler):
-    def get(self):
-        file = open('index.html', 'r')
-        self.response.out.write(file.read())
-
-app = webapp2.WSGIApplication([('/', HomePageHandler)], debug=True)
