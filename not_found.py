e7b6b7ef07fe2a92266bffc1e201af5a54e81cc8
Andy Throgmorton <athrog@users.noreply.github.com>
2022-01-19 17:26:32 -0800

    Delete not_found.py

--- not_found.py
+++
@@ -1,8 +0,0 @@
-import webapp2
-
-class PageNotFoundHandler(webapp2.RequestHandler):
-    def get(self):
-        file = open('not_found.html', 'r')
-        self.response.out.write(file.read())
-
-app = webapp2.WSGIApplication([('/.*', PageNotFoundHandler)], debug=True)
